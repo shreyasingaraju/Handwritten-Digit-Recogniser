a1c34d8d4bfb1670f76531eda7b02d9f4abe26f3
hmit106 <68830694+hmit106@users.noreply.github.com>
2021-04-09 18:59:08 +1200

    Changed ProjectGUI from a QWidget to a QMainWindow to use the menubar etc.
    Also made the py file for the ProjectModel module

--- ProjectGUI.py
+++ ProjectGUI.py
@@ -1,30 +1,52 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QProgressBar, QGridLayout, QLabel, QFileDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QProgressBar, QGridLayout, QLabel, QFileDialog, QMainWindow, QAction, qApp
 from PyQt5.QtCore import QBasicTimer, QPoint
-from PyQt5.QtGui import QPainter, QBrush, QPen, QPixmap
+from PyQt5.QtGui import QPainter, QBrush, QPen, QPixmap, QColor, QIcon
 
-class ProjectGUI(QWidget):
+class ProjectGUI(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
         grid = QGridLayout()
-        self.setLayout(grid)
+        # self.setLayout(grid)
+        window = QWidget(self)
+        window.setLayout(grid)
+        self.setCentralWidget(window)
+
+        # Add the file menubar
+        exitAction = QAction(QIcon('D:\workspace\COMPSYS302_PyQt5\exit.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        openTrainModelDialog = QAction(QIcon('D:\workspace\COMPSYS302_PyQt5\exit.png'), 'Train Model', self)
+        openTrainModelDialog.setShortcut('Ctrl-t')
+        openTrainModelDialog.setStatusTip('Train the Model')
+        openTrainModelDialog.triggered.connect(self.trainModelDialog)
+
+        menubar = self.menuBar()
+        filemenu = menubar.addMenu('&File')
+        filemenu.addAction(exitAction)
+        filemenu.addAction(openTrainModelDialog)
+
         grid.addWidget(QPushButton('TODO: make this a title instead of a button'),0,0)
         
         self.drawing_box = QLabel()
         grid.addWidget(self.drawing_box,1,0)
         self.pen = QPen()
         self.pen.setWidth(5)
-        self.canvas = QPixmap("./testimage.png")
+        self.canvas = QPixmap(280,280)
+        self.canvas.fill(QColor(255,255,255))
         self.drawing_box.setPixmap(self.canvas)
 
         clear_button = QPushButton("Clear")
         grid.addWidget(clear_button,0,1)
 
-        self.setWindowTitle('QGridLayout')
+        self.setWindowTitle('Digit Recogniser')
         self.setGeometry(300, 300, 300, 200)
+
         self.show()
 
     # In order to draw numbers such as 4 we have to be able to "lift the pen" off of the canvas
@@ -42,10 +64,26 @@ class ProjectGUI(QWidget):
         self.update()
 
     def mouseReleaseEvent(self, e):
+        self.update()
         # Save the image when the user releases the mouse
         # Doesn't currently work, possibly because I'm drawing on the Qlabel rather than the QPixMap or something idk
         self.canvas.save("drawnimage.png")
         print("Saved as drawnimage.png (Saving not currently working)")
+
+    def trainModelDialog(self, s):
+        dialog = TrainingWindow()
+
+class TrainingWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        self.setGeometry(200, 200, 200, 200)
+        self.show()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
---
+++ ProjectModel.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision
+
+class ProjectModel
+    def __init__(self):
+        
+    def TrainModel(dataset):
+        
